d9fe5d3440087f8ffc9b379ebfcccc4c315945cd
Xavier Andueza <xavier.d.andueza@gmail.com>
2023-08-20 14:44:03 +1000

    Analysis in Python, more analysis in C#

---
+++ Analysis/2023-08-15/GetAngles.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Neurorehab.Analysis.2023-08-15
+{
+    public class GetAngles
+    {
+        
+    }
+}
