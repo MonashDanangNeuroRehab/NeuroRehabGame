406c3ffc9e1a9e883367a2b061b8306164003ccf
Xavier Andueza <xand0001@student.monash.edu>
2023-10-09 19:06:51 +1100

    Smoothness Analysis in C#, not quite done yet.

--- Assets/Scripts/MovementAnalysis/AnalyseMovements.cs
+++ Assets/Scripts/MovementAnalysis/AnalyseMovements.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -17,7 +18,7 @@ public class AnalyseMovements : MonoBehaviour
 
     // This has a series of functions that are used to analyse vectors
     /// <summary> 
-    /// Fnds the maximum and minimum angles achieved by the joint, over the repetitions
+    /// Finds the maximum and minimum angles achieved by the joint, over the repetitions
     /// </summary>
     /// <param name="angles">A list of floats which are the angles that have been achieved.</param>
     /// <returns>A 2D List, List<float>[2] - The first list is the time which is the min angles, the latter of which is the max angles
@@ -136,4 +137,90 @@ public class AnalyseMovements : MonoBehaviour
 
         return minMaxAngles;
     }
+
+    public List<float> CalculateAngularVelocities(List<float> angles, float timestep)
+    {
+        List<float> angularVelocities = new List<float>();
+        for (int i = 0; i < angles.Count - 1; i++)
+        {
+            float angularVelocity = (angles[i + 1] - angles[i]) / timestep;
+            angularVelocities.Add(angularVelocity);
+        }
+        return angularVelocities;
+    }
+
+    public List<float> ApplyMovingAverage(List<float> angles, int movingAverageParam)
+    {
+        List<float> smoothedAngles = new List<float>();
+
+        // Ensure the moving average parameter is valid
+        if (movingAverageParam <= 0 || movingAverageParam > angles.Count)
+        {
+            return angles;  // Return the original list if the parameter is invalid
+        }
+
+        for (int i = 0; i < angles.Count; i++)
+        {
+            float sum = 0;
+            int count = 0;
+
+            // Calculate the start and end indices for the moving average window
+            int startIdx = i - movingAverageParam / 2;
+            int endIdx = i + movingAverageParam / 2;
+
+            for (int j = startIdx; j <= endIdx; j++)
+            {
+                // Check for boundary conditions
+                if (j >= 0 && j < angles.Count)
+                {
+                    sum += angles[j];
+                    count++;
+                }
+            }
+
+            smoothedAngles.Add(sum / count);
+        }
+
+        return smoothedAngles;
+    }
+
+    public List<float> PadAngularVelocities(List<float> angularVelocities, int padToNumber)
+    {
+        // Check if padding is needed
+        int paddingAmount = padToNumber - angularVelocities.Count;
+
+        for (int i = 0; i < paddingAmount; i++)
+        {
+            angularVelocities.Add(0.0f);
+        }
+
+        return angularVelocities;
+    }
+
+    /// <summary> 
+    /// Uses Spectral Arc-Length on a Fourier Transform to evaluate the smoothness of the inputted angles
+    /// </summary>
+    /// <param name="angles">A list of floats which are the angles that have been achieved.</param>
+    /// <returns>A float, which is the measure of the motion smoothness
+    public float EvaluateSmoothness(List<float> angles)
+    {
+        // Evaluates the angles
+        float timestep = 1.0f / 60.0f;
+        int movingAverageParam = 6; // 6 because we have 4.5Hz * 2, so 9Hz to detect up to 4.5Hz. 60FPS therefore 6FPS smoothing
+
+        List<float> smoothedAngles = ApplyMovingAverage(angles, movingAverageParam);
+
+        // Call calculate angular velocities function
+        List<float> angularVelocities = CalculateAngularVelocities(smoothedAngles, timestep);
+
+        // Pad my smoothed angles
+        int exponent = (int)Math.Ceiling((Math.Log(angularVelocities.Count) / Math.Log(2.0f)) + 4.0f);
+        int padToNumber = (int)(Math.Pow(2.0f, exponent)
+
+        List<float> paddedAngularVelocities = PadAngularVelocities(angularVelocities, padToNumber)
+
+
+    }
+
+
 }
